fc592bc1ac90b0b04166fa89dc1abb247a258f01
MayurArchives <mayurkanyal10@gmail.com>
2024-07-23 01:21:50 +0530

    hii hello series

--- 4.js
+++ 4.js
@@ -1,1 +1,9 @@
-console.log("kello");
+let arr =["hi","hello","bye","!"];
+function concat(str) {
+    let result = " ";
+    for (let i = 0; i < str.length; i++) {
+        result +=str[i]; 
+    }
+    return result;
+    
+}
