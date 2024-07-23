f2372f08e56e5e7538ff286b5543635126dd2f06
MayurArchives <mayurkanyal10@gmail.com>
2024-07-23 23:48:50 +0530

    maph info

---
+++ 9.js
@@ -0,0 +1,21 @@
+let arr =[
+
+    {
+    name :"mayur",
+    marks: 88,   
+    
+    },
+    {
+    name :"milind",
+    marks: 90,   
+    
+    },
+    {
+    name :"mohoit",
+    marks: 99, 
+    },
+    ];
+let gpa = student.map((el)=>{
+    return marks/10;
+
+});
