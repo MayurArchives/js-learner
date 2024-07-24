24a3e867d113837f30b3f207bdf64079fcf97cd5
MayurArchives <mayurkanyal10@gmail.com>
2024-07-24 23:42:16 +0530

    default parameter

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+let arr =[
+
+{
+name :"mayur",
+marks: 88,   
+
+},
+{
+name :"milind",
+marks: 90,   
+
+},
+{
+name :"mohoit",
+marks: 99, 
+},
+];
+arr.forEach((student) =>{
+    console.log(student);
+});
