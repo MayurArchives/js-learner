b65629e4ae41fe37d95b8fd1c20c9dd32dcce1d5
MayurArchives <mayurkanyal10@gmail.com>
2024-07-26 23:42:41 +0530

    spreaad liter

--- 13.js
+++ 13.js
@@ -1,6 +1,6 @@
 let arr = [1,2,3,4,5];
 let newArr = [...arr];
 let char = ["hello"];
-let odd =[1,3,5,7,9];
+let odd =[1,3,5,7,9,11];
 let even = [2,3,4,5,6];
-let nums = [...odd,...even]
+let nums = [...even,...odd]
