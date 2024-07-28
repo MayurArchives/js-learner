102567d54361f5d52719600c1e40cf0044c4d4be
MayurArchives <mayurkanyal10@gmail.com>
2024-07-28 22:59:52 +0530

    function apply

---
+++ 14.js
@@ -0,0 +1,10 @@
+const person = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  const person1 = {
+    firstName: "Mary",
+    lastName: "Doe"
+  }
