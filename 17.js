6b1db2473e67c744e2e4c6933766c15ec6c3e290
MayurArchives <mayurkanyal10@gmail.com>
2024-08-06 22:59:24 +0530

    Include style.css and prof folder

---
+++ 17.js
@@ -0,0 +1,6 @@
+let div= document.querySelector("div");
+div.style.background = "green";
+div.style.backgroundColor = "blue";
+div.style.visibility ="hidden";
+div.style.fontSize = "26px";
+div.innerText = "Hello";
