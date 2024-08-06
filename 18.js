ef7264dccfade8fddd07888ae945b597ee4fcccc
MayurArchives <mayurkanyal10@gmail.com>
2024-08-07 00:09:00 +0530

    Include style.css and prof folder

---
+++ 18.js
@@ -0,0 +1,7 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!";
+
+newBtn.style.background = "white";
+newBtn.style.background.color = "purple";
+
+document.querySelector("body").prepend(newBtn);
