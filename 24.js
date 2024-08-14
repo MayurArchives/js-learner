ad78c5057c680bb05e6ffaf7cc345348d2f95176
MayurArchives <mayurkanyal10@gmail.com>
2024-08-14 12:37:42 +0530

    fetch api

--- 24.js
+++ 24.js
@@ -1,13 +1,13 @@
-// const getFetchData = async () => {
-//     console.log("getting data...");
+const getFetchData = async () => {
+    console.log("getting data...");
 
-//     let res = await fetch("https://jsonplaceholder.typicode.com/comments")
-//     // console.log(res);
-//     let data = await res.json();
-//     console.log(data);
-// }
-// getFetchData();
+    let res = await fetch("https://jsonplaceholder.typicode.com/comments")
+    // console.log(res);
+    let data = await res.json();
+    console.log(data);
+}
+getFetchData();
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
