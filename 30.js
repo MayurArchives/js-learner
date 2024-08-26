c30b2eeacd1c1239f2b2057cafa5c666184e1710
MayurArchives <mayurkanyal10@gmail.com>
2024-08-27 02:53:06 +0530

    function

---
+++ 30.js
@@ -0,0 +1,46 @@
+// function brotherHello() {
+//     console.log("hello,Bro!");
+    
+// }
+// brotherHello();
+// basic function Declaration
+
+// 2. Function with Parameters
+// function  buddy(name) {
+//     console.log("hello," + name + "!");
+    
+// }
+// buddy("mayur");
+// buddy("milind");
+
+
+// Function with Argument
+// function printInfo (name,age){
+//     console.log(`${name} age is ${age}`);
+    
+// }
+// printInfo("mayur","22");
+
+
+// 4. Function with Return Value
+// function add(a,b) {
+//     return a+b;
+
+// }
+// let sum = add(2,6);
+// console.log(sum);
+
+
+// Arrow Function:
+// const add = (a,b)=> a+b;
+// console.log(add(2,3));
+
+
+const multlipy = function(a, b){
+    return a * b;
+
+}
+console.log(multlipy(3,3));
+
+
+
