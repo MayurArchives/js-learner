79cbf9fdaaec740e263944e01259aa1d6f37f02d
MayurArchives <mayurkanyal10@gmail.com>
2024-09-01 00:29:26 +0530

    challenge

---
+++ tl.js
@@ -0,0 +1,18 @@
+document.getElementById('addTodoBtn').onclick = () =>{
+    const todoText = document.getElementById('todoInput').value.trim();
+    if (!todoText) return alert('Please enter a task!');
+    console.log(`Task Added: ${todoText}`);
+
+const listItem = document.createElement('li');
+listItem.innerHTML = todoText; 
+
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'Delete';
+deleteBtn.className = 'deleteBtn';
+
+
+listItem.appendChild(deleteBtn);
+deleteBtn.onclick = () => listItem.remove();
+document.getElementById('todoInput').value = '';
+
+};
