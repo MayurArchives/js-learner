d4fff427f759180b3e734fb1d9ed2752c2c40ed6
MayurArchives <mayurkanyal10@gmail.com>
2024-09-04 22:39:29 +0530

    challenge

--- 38.js
+++ 38.js
@@ -1,7 +1,30 @@
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', () =>{
-    let name = 'mayur kanyal';
-    console.log(name.toUpperCase());
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click',() => {
+//     let name = 'mayur kanyal';
+//     console.log(name.toUpperCase());
     
 
+// });
+
+// promises
+setTimeout(() => {
+    console.log("mayur");
+    setTimeout(() => {
+        console.log("milind");
+        setTimeout(() => {
+            console.log("aishwary");
+            setTimeout(() => {
+                console.log("ujjwal");
+                setTimeout(() => {
+                    console.log("akshay");
+                    setTimeout(() => {
+                        console.log("akshit");
+                    },2000);
+                },3000);
+            },4000);
+        },5000);
+    },2000);
+
+
+    
 });
