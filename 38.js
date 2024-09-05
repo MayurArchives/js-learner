e47f0f5ac014faaac6621ce927587412eba1dbae
MayurArchives <mayurkanyal10@gmail.com>
2024-09-05 08:48:12 +0530

    challenge

--- 38.js
+++ 38.js
@@ -7,24 +7,41 @@
 // });
 
 // promises
-setTimeout(() => {
-    console.log("mayur");
-    setTimeout(() => {
-        console.log("milind");
-        setTimeout(() => {
-            console.log("aishwary");
-            setTimeout(() => {
-                console.log("ujjwal");
-                setTimeout(() => {
-                    console.log("akshay");
-                    setTimeout(() => {
-                        console.log("akshit");
-                    },2000);
-                },3000);
-            },4000);
-        },5000);
-    },2000);
+// setTimeout(() => {
+//     console.log("mayur");
+//     setTimeout(() => {
+//         console.log("milind");
+//         setTimeout(() => {
+//             console.log("aishwary");
+//             setTimeout(() => {
+//                 console.log("ujjwal");
+//                 setTimeout(() => {
+//                     console.log("akshay");
+//                     setTimeout(() => {
+//                         console.log("akshit");
+//                     },2000);
+//                 },3000);
+//             },4000);
+//         },5000);
+//     },2000);
 
 
     
+// });
+
+const myPromise = new Promise((resolve, reject) => {
+    let connection = true;
+    if (connection){
+        resolve("connection is established")
+    
+    }
+    else {
+        reject("connection is not established")
+    }
+});
+
+myPromise.then(()=>{
+    console.log(myPromise);
+    
+
 });
