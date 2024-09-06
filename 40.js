d43aed84560f8cbfe27833ec015901f5b55c3214
MayurArchives <mayurkanyal10@gmail.com>
2024-09-06 12:20:00 +0530

    filter

--- 40.js
+++ 40.js
@@ -1,5 +1,20 @@
-var arr = [4, 10, 8];
-var filterMethod = arr.filter(e => e > 4);
-console.log(filterMethod);
-var finddMethod = arr.find(e => e > 4)
-console.log(finddMethod);
+// var arr = [4, 10, 8];
+// var filterMethod = arr.filter(e => e > 4);
+// console.log(filterMethod);
+// var finddMethod = arr.find(e => e > 4)
+// console.log(finddMethod);
+const users = [
+    { name: 'mayur', age: 14 },
+    { name: 'milind', age: 52 },
+    { name: 'aish', age: 56 },
+    { name: 'ujjwal', age: 18 },
+];
+
+const youngPeople = users.filter((person) => {
+    return person.age <= 15;
+});
+const seniorPeople = users.filter((person) => person.age >= 50);
+
+console.log(seniorPeople);
+
+console.log(youngPeople);
