521b0dff2d7096fa96a71f12bd3f94fe81f530b4
MayurArchives <mayurkanyal10@gmail.com>
2024-09-06 23:54:13 +0530

    destructuring

--- 41.js
+++ 41.js
@@ -1,7 +1,12 @@
-let arr = [3, 4, 5, 6];
-let modifiedArr = arr.map(function (element) {
-    return element * 3;
+// let arr = [3, 4, 5, 6];
+// let modifiedArr = arr.map(function (element) {
+//     return element * 3;
 
 
-});
-console.log(modifiedArr);
+// });
+// console.log(modifiedArr);
+let introduction = ["Hello", "I", "am", "Sarah"];
+let [greeting, pronoun] = introduction;
+
+console.log(greeting);//"Hello"
+console.log(pronoun);
