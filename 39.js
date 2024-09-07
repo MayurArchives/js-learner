10aad89895d7d3bcdc7a910fdcd20b4a520361c1
MayurArchives <mayurkanyal10@gmail.com>
2024-09-07 23:59:27 +0530

    spread operator

--- 39.js
+++ 39.js
@@ -1,8 +1,15 @@
-function addNumbers(a, b, c, ...others) {
-    console.log(others);
-    return a + b + c;
+// function addNumbers(a, b, c, ...others) {
+//     console.log(others);
+//     return a + b + c;
 
+
+// }
+// const res = addNumbers(2, 3, 4, 5, 6, 7, 8)
+// console.log(res);
+
+var names = [" aman", "suresh", "vicky", "ramesh"];
+function getNames(name1, name2, name3, name4) {
+    console.log(name1, name2, name3, name4);
 
 }
-const res = addNumbers(2, 3, 4, 5, 6, 7, 8)
-console.log(res);
+getNames(...names)
