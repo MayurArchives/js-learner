2a28d4658d89f5fe146443a221f1037f93b833c0
MayurArchives <mayurkanyal10@gmail.com>
2024-09-08 20:33:46 +0530

    rest and spread  operator

--- 43.js
+++ 43.js
@@ -1,7 +1,11 @@
-var students = {
-    name: "Mayur",
-    age: "22",
-    hobbies: ["gym", "playing"]
-};
-const { ...rest } = students;
-console.log(rest)
+// var students = {
+//     name: "Mayur",
+//     age: "22",
+//     hobbies: ["gym", "playing"]
+// };
+// const { ...rest } = students;
+// console.log(rest)
+
+const Number = [1, 2, 3, 4, 5, 6, 7];
+const moreNumber = [...Number];
+console.log(moreNumber);
