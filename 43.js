9e6217d2fbf286d9705a6d515528a44f772f2933
MayurArchives <mayurkanyal10@gmail.com>
2024-09-08 20:36:19 +0530

    rest and spread  operator

--- 43.js
+++ 43.js
@@ -6,6 +6,12 @@
 // const { ...rest } = students;
 // console.log(rest)
 
-const Number = [1, 2, 3, 4, 5, 6, 7];
-const moreNumber = [...Number];
-console.log(moreNumber);
+// const Number = [1, 2, 3, 4, 5, 6, 7];
+// const moreNumber = [...Number];
+// console.log(moreNumber);
+
+const [first, second, ...rest] = [1, 2, 3, 4, 5, 6, 7];
+console.log(second);
+console.log(rest);
+
+
