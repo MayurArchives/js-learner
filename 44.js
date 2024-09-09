d69d5a910525a4b882618952741a16588c4ea841
MayurArchives <mayurkanyal10@gmail.com>
2024-09-09 11:54:40 +0530

    reduce method

--- 44.js
+++ 44.js
@@ -1,4 +1,9 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7];
-const sum = numbers.reduce((accumulator, currentValue) =>
-    accumulator + currentValue, 0);
-console.log(sum);
+// const numbers = [1, 2, 3, 4, 5, 6, 7];
+// const sum = numbers.reduce((accumulator, currentValue) =>
+//     accumulator + currentValue, 0);
+// console.log(sum);
+
+const numbers = [10, 20, 45, 67, 800, 670];
+const max = numbers.reduce((accum, currVal) =>
+    currVal > accum ? currVal : accum, 0);
+console.log(max);
