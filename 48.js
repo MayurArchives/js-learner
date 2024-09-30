1d411cf9566aef3d1e051d7c702612a184620913
MayurArchives <mayurkanyal10@gmail.com>
2024-09-30 11:48:50 +0530

    sum

--- 48.js
+++ 48.js
@@ -9,8 +9,12 @@
 // console.log(arr);
 
 let myArray = [1, 2, 3, 4, 5];
+let sum = 0;
 for (let i = 0; i < myArray.length; i++) {
     myArray[i] += 1;
+    sum += myArray[i]
 }
 console.log(myArray);
+console.log(sum);
+
 
