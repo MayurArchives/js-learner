42179ce83718851b553721a6ce506e11110f1d4c
MayurArchives <mayurkanyal10@gmail.com>
2024-10-01 23:47:27 +0530

    sum

--- 48.js
+++ 48.js
@@ -8,13 +8,40 @@
 // let arr = new Array(5).fill(0)
 // console.log(arr);
 
-let myArray = [1, 2, 3, 4, 5];
-let sum = 0;
+// let myArray = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let i = 0; i < myArray.length; i++) {
+//     myArray[i] += 1;
+//     sum += myArray[i]
+// }
+// console.log(myArray);
+// console.log(sum);
+// let num = 2;
+// let myArray = [];
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(num * i);
+//     myArray.push(num * i)
+
+
+
+// }
+// console.log(myArray);
+
+
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8];
 for (let i = 0; i < myArray.length; i++) {
-    myArray[i] += 1;
-    sum += myArray[i]
+    if (myArray[i] % 1 === 0) {
+        myArray[i] += 5;
+
+
+    }
 }
 console.log(myArray);
-console.log(sum);
+
+
+
+
 
 
