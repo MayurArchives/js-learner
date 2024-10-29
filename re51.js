9f6887b9ed9de54fbdd77e6065a0ff6995f3977e
MayurArchives <mayurkanyal10@gmail.com>
2024-10-29 14:08:57 +0530

    using templete literals

--- re51.js
+++ re51.js
@@ -3,8 +3,13 @@
 // firstName = "saurabh";
 // console.log(firstName);
 
-let string1 = "17";
-let newString2 = "24";
-let newString = +string1 + +newString2;
+// let string1 = "17";
+// let newString2 = "24";
+// let newString = +string1 + +newString2;
 
-console.log(typeof newString);
+// console.log(typeof newString);
+let age = "22";
+let firstName = "Mayur";
+let aboutMe = `my name is ${firstName} and  my age is ${age}`
+console.log(aboutMe);
+
