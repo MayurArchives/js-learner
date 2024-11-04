332d22b4d8653abb23d049291b454e8b0dd3c505
MayurArchives <mayurkanyal10@gmail.com>
2024-11-04 19:53:01 +0530

    array

--- re53.js
+++ re53.js
@@ -1,5 +1,5 @@
 let array1 = ["item1", "item2", "item3"];
-let array2 = [...array1];
+let array2 = [...array1, "item4", "item5"];
 array1.push("item4");
 
 console.log(array1 === array2);
---
+++ re54.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "harshit",
+    age: 22,
+    hobbies: ["guitar", "sleeping", "gym", "cricket"]
+
+}
+console.log(person);
+console.log(person.cricket);
+person.gender = "male";
+console.log(person);
+
+
