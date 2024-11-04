14d47b1a8b9112c7d8332eb80f0cb04fa7c2b2cc
MayurArchives <mayurkanyal10@gmail.com>
2024-11-04 18:00:54 +0530

    spread op in  array

--- re53.js
+++ re53.js
@@ -1,3 +1,8 @@
 let array1 = ["item1", "item2", "item3"];
-let array2 = array1;
+let array2 = [...array1];
+array1.push("item4");
+
 console.log(array1 === array2);
+console.log(array1);
+console.log(array2);
+
