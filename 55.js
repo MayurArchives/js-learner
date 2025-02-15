aed51aa8d4111a3a01054a9aac1b0ac39e26005c
MayurArchives <mayurkanyal10@gmail.com>
2025-02-15 18:47:48 +0530

    string reversal

---
+++ 55.js
@@ -0,0 +1,13 @@
+// 1. String Reversal (Without Built-in Reverse Function)
+
+
+
+function reverseString(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+console.log(reverseString("hello")); // Output: "olleh"
