af61e3443a5ac6e8ab27f4de72d666e400b5f4d9
MayurArchives <mayurkanyal10@gmail.com>
2025-02-17 23:52:05 +0530

    Decode String

---
+++ 65.js
@@ -0,0 +1,30 @@
+// Decode String
+
+
+
+var decodeString = function (s) {
+
+    const stack = []
+    for (const char of s) {
+        if (char !== "]") {
+            stack.push(char)
+            continue
+        }
+        let currentChar = stack.pop()
+        let innerString = ""
+        while (currentChar !== "[") {
+            innerString = currentChar + innerString
+            currentChar = stack.pop()
+        }
+        currentChar = stack.pop()
+        let multiplier = ""
+        while (!Number.isNaN(Number(currentChar))) {
+            multiplier = currentChar + multiplier
+            currentChar = stack.pop()
+        }
+        stack.push(currentChar)
+        stack.push(innerString.repeat(Number(multiplier)))
+    }
+
+    return stack.join('')
+};
